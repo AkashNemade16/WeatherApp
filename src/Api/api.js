0101014a284c174d9def94d54fa5e30a988435e4
Akash Nemade <37910311+AkashNemade16@users.noreply.github.com>
2021-09-13 11:33:40 -0700

    api folder delete

--- src/Api/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const WeatherData=()=>(
-  axios.get('api.openweathermap.org/data/2.5/weather?q={NewYork}&appid={cb7277d5ec140b37517cb73278e42157}')
-  .then(res=>console.log(res))
-  .catch(err=>console.log(err))
-)
